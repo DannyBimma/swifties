af50f8ddb0789f8e45087c10afaa70ccc0979038
DannyBimma <dantrotman92@gmail.com>
2025-10-13 19:01:07 -0400

    Fix type casting error.

--- typeshit.swift
+++ typeshit.swift
@@ -3,7 +3,6 @@
 let anum = 2025
 let anumDayCount = 286
 let anumDayTotal = 365
-let anumPercentage: Double = (anumDayCount / anumDayTotal) * 100
+let anumPercentage: Double = (Double(anumDayCount) / Double(anumDayTotal)) * 100
 
-print("\n")
 print("The year \(anum) is exactly \(anumPercentage) completed!")
