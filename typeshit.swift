766637f42f122ed1c5bd9d44fae1cec3f4ec4f5c
DannyBimma <dantrotman92@gmail.com>
2025-10-13 19:13:50 -0400

    Make current year dynamic.

--- typeshit.swift
+++ typeshit.swift
@@ -4,9 +4,9 @@
 // Link Foundation Framework for date/time classes and such
 import Foundation
 
-let anum = 2025
+let anum = Calendar.current.component(.year, from: Date())
 let anumDayCount = Calendar.current.ordinality(of: .day, in: .year, for: Date()) ?? 1
 let anumDayTotal = 365
 let anumPercentage: Double = (Double(anumDayCount) / Double(anumDayTotal)) * 100
 
-print("The year \(anum) is exactly \(anumPercentage) completed!")
+print("The year \(anum) is exactly \(anumPercentage)% completed!")
