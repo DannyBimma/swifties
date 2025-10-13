077f09015068aa37d83b701d27b1df1a2ebea9f8
DannyBimma <dantrotman92@gmail.com>
2025-10-13 19:11:00 -0400

    Make current day dynamic.

--- typeshit.swift
+++ typeshit.swift
@@ -1,7 +1,11 @@
-// A Look at How Types Work in Swift
+// A Look at the Basics:
+// Types, Casting, Standard Lib, Class/Object Methods
+
+// Link Foundation Framework for date/time classes and such
+import Foundation
 
 let anum = 2025
-let anumDayCount = 286
+let anumDayCount = Calendar.current.ordinality(of: .day, in: .year, for: Date()) ?? 1
 let anumDayTotal = 365
 let anumPercentage: Double = (Double(anumDayCount) / Double(anumDayTotal)) * 100
 
