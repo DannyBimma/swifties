9053232e9b1b0683a85c0d54d3f6c6378ed5f00b
DannyBimma <dantrotman92@gmail.com>
2025-10-13 18:56:12 -0400

    Try to understand typing casting etc..

---
+++ typeshit.swift
@@ -0,0 +1,9 @@
+// A Look at How Types Work in Swift
+
+let anum = 2025
+let anumDayCount = 286
+let anumDayTotal = 365
+let anumPercentage: Double = (anumDayCount / anumDayTotal) * 100
+
+print("\n")
+print("The year \(anum) is exactly \(anumPercentage) completed!")
