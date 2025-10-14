61107a153d8c1df747c376d3770714e4fd6ac2e8
DannyBimma <dantrotman92@gmail.com>
2025-10-14 12:06:05 -0400

    Trying to extract values.

--- arrs_n_dicts.swift
+++ arrs_n_dicts.swift
@@ -1,4 +1,23 @@
 // Arrays and Dictionaries
 
-// Commas are allowed after the last element
+// Initial array of original crew members
 var strawHats = ["Luffy", "Zoro", "Nami", "Usopp"]
+
+// Dictionary for crew members and their roles
+var crewRoles = [
+    "Luffy": "Captain",
+    "Zoro": "Vice Captain",
+    "Nami": "Navigator",
+    "Usopp": "Sniper",
+]
+
+// Add elements to array, they automatically grow
+strawHats.append("Chopper")
+
+// Same for the dictionaries
+crewRoles["Chopper"] = "Doctor"
+
+print(strawHats)
+print(crewRoles)
+print(strawHats[0])
+print(crewRoles["Luffy"])
