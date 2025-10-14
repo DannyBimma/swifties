85c7843d032883898cf7ebce058af4972e67b84e
DannyBimma <dantrotman92@gmail.com>
2025-10-14 15:48:02 -0400

    Output conditional value.

--- controlflow.swift
+++ controlflow.swift
@@ -1,3 +1,14 @@
 // Loops, Iterators, and Conditionals
 
 let salesHistory = [420.69, 42.47, 360.00, 195.55, 99.99]
+var commission = 0
+
+for sale in salesHistory {
+    if sale > 50 {
+        commission += 15
+    } else {
+        commission += 5
+    }
+}
+
+print(commission)
