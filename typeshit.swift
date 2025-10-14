87911c17e4c863ef13f8ebcde9fe962914302186
DannyBimma <dantrotman92@gmail.com>
2025-10-13 21:18:23 -0400

    Try to format decimal places.

--- typeshit.swift
+++ typeshit.swift
@@ -7,6 +7,13 @@ import Foundation
 let anum = Calendar.current.component(.year, from: Date())
 let anumDayCount = Calendar.current.ordinality(of: .day, in: .year, for: Date()) ?? 1
 let anumDayTotal = 365
-let anumPercentage: Double = (Double(anumDayCount) / Double(anumDayTotal)) * 100
+var anumPercentage: Double = (Double(anumDayCount) / Double(anumDayTotal)) * 100
+
+// Format output value to 3 decimal places, based on current locale
+let formatter = NumberFormatter()
+formatter.numberStyle = .decimal
+formatter.maximumFractionDigits = 3  // Decimal places
+formatter.minimumFractionDigits = 3  // Guard against trailing 0's
+anumPercentage = formatter.string(from: NSNumber(value: anumPercentage))
 
 print("The year \(anum) is exactly \(anumPercentage)% completed!")
