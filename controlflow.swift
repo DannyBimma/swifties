30da785fd6d6e8948fd63c8400b06792a5bfce1c
DannyBimma <dantrotman92@gmail.com>
2025-10-14 16:46:19 -0400

    Explore conditional limits.

--- controlflow.swift
+++ controlflow.swift
@@ -1,5 +1,28 @@
 // Loops, Iterators, and Conditionals
 
+// Optionals for value that may be missing
+let timeofDay: Int? = 9
+let userName: String? = "Danny Bimma"
+
+// Set default value for potential missing/nil optional values
+var greeting = ""
+
+if let user = userName {
+    if let time = timeofDay {
+        if timeofDay ?? 12 < 11 {
+            greeting = "Good morning, \(user)!"
+        } else if timeofDay ?? 12 > 12 {
+            greeting = "Good evening \(user)!"
+        } else {
+            greeting = " Good day, \(user)!"
+        }
+    }
+}
+
+print("SALES ASSESMENT FOR CURRENT MONTH:")
+print("\n")
+print(greeting)
+
 let salesHistory = [420.69, 42.47, 360.00, 195.55, 99.99]
 var commission = 0
 
@@ -11,6 +34,7 @@ for sale in salesHistory {
     }
 }
 
+// Conditional constant assignment
 let salesAssesment =
     if commission > 40 {
         "Excellent, you are actually capable of selling wares!"
@@ -20,3 +44,4 @@ let salesAssesment =
 
 print("Sales Commission: $\(commission)")
 print("SaleBot Assesment: \(salesAssesment)")
+print("\n")
