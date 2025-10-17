c3e640c700ac7880135abc82f5ae376f723ccec9
DannyBimma <dantrotman92@gmail.com>
2025-10-16 21:02:07 -0400

    Use more idiomatic unwrapping.

--- controlflow.swift
+++ controlflow.swift
@@ -5,18 +5,21 @@ let timeofDay: Int? = 9
 let userName: String? = "Danny Bimma"
 var greeting = ""
 
-// Idiomatically unwrap both values in nesting
-if let user = userName {
-    if let time = timeofDay {
-        if time < 11 {
-            greeting = "Good morning, \(user)!"
-        } else if time > 12 {
-            greeting = "Good evening \(user)!"
-        } else {
-            greeting = " Good day, \(user)!"
-        }
+// Generate greeting using idiomatic guard let for early exit
+func generateGreeting(user: String?, time: Int?) -> String {
+    guard let user = user else { return "" }
+    guard let time = time else { return "" }
+
+    if time < 11 {
+        return "Good morning, \(user)!"
+    } else if time > 12 {
+        return "Good evening \(user)!"
+    } else {
+        return "Good day, \(user)!"
     }
 }
+
+greeting = generateGreeting(user: userName, time: timeofDay)
 
 print("SALES ASSESMENT FOR CURRENT MONTH:")
 print("\n")
