fd8cc9714e3c0d4cf2288a2e57cdc8355ed18a56
DannyBimma <dantrotman92@gmail.com>
2025-10-17 22:18:29 -0400

    Write multi-return func.

--- views.swift
+++ views.swift
@@ -1,3 +1,20 @@
 //Functions and their Scope
 
-func viewCounter(views: [Int]) -> (min: Int, max: Int, total: Int) {}
+// Return multiple values from a function by using a tuple
+func viewCounter(views: [Int]) -> (min: Int, max: Int, total: Int) {
+    var min = views[0]
+    var max = views[0]
+    var total = 0
+
+    for view in views {
+        if view > max {
+            max = view
+        } else if view < min {
+            min = view
+        }
+
+        total += view
+    }
+
+    return (min, max, total)
+}
