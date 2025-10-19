050761f51cf8852a3cd86b75238b001f45e32bdf
DannyBimma <dantrotman92@gmail.com>
2025-10-18 21:31:20 -0400

    Create anime class with initializer.

--- object_oriented.swift
+++ object_oriented.swift
@@ -1,1 +1,16 @@
 // Object Oriented Programming in Swift
+
+class Anime {
+    var title: String
+    var epCount: Int = 0
+    var rating: Int = 0
+    var currentlyAiring: Bool
+
+    init(title: String, epCount: Int, rating: Int, currentlyAiring: Bool) {
+        self.title = title
+        self.epCount = epCount
+        self.rating = rating
+        self.currentlyAiring = currentlyAiring
+    }
+}
+
