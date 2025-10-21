f10f27eea364584c8bcd54c6d94c239f4b723a1a
DannyBimma <dantrotman92@gmail.com>
2025-10-21 18:12:58 -0400

    Create enum to rep the crew.

--- enumerate.swift
+++ enumerate.swift
@@ -1,2 +1,9 @@
 // Like classes and all other named types, enumerations
 // in Swift can have methods associated with them.
+
+enum strawHats {
+    case Luffy
+    case Zoro
+    case Usopp
+    case Nami
+}
