b7ff8ba166bdbdf708821e4b2fcb7a27757e1ae9
DannyBimma <dantrotman92@gmail.com>
2025-10-21 18:43:18 -0400

    Add crew roles method.

--- enumerate.swift
+++ enumerate.swift
@@ -5,6 +5,23 @@
 enum strawHats {
     case Luffy
     case Zoro
+    case Sanji
     case Usopp
     case Nami
+
+    // A method to returns crew roles
+    func role() -> String {
+        switch self {
+        case .Luffy:
+            return "Captain"
+        case .Zoro:
+            return "Vice Captain"
+        case .Usopp:
+            return "Sniper"
+        case .Sanji:
+            return "Cook"
+        case .Nami:
+            return "Navigator"
+        }
+    }
 }
