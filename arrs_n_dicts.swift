01fd7b9f7d2302f09ae5b1f109b026e7dedc4492
DannyBimma <dantrotman92@gmail.com>
2025-11-12 22:09:16 -0400

    idk part 2.

--- arrs_n_dicts.swift
+++ arrs_n_dicts.swift
@@ -7,6 +7,7 @@ var strawHats = ["Luffy", "Zoro", "Nami", "Usopp"]
 var crewRoles = [
     "Luffy": "Captain",
     "Zoro": "Vice Captain",
+    "Sanji": "Chef",
     "Nami": "Navigator",
     "Usopp": "Sniper",
 ]
@@ -16,15 +17,23 @@ strawHats.append("Chopper")
 
 // Same for the dictionaries
 crewRoles["Chopper"] = "Doctor"
+crewRoles["Franky"] = "Shipwright"
+crewRoles["Robin"] = "Archaeologist"
+crewRoles["Jinbei"] = "Helmsman"
 
 print(strawHats)
+print()
 print(strawHats[0])
+print()
+print(crewRoles)
+print()
 // Provide default value if key doesn't exist
 // Dicts always return optionals types
 // the nil-coalescing operator (??) which
 // explicitly handles the optional and provides
 // a default value.
 print(crewRoles["Luffy"] ?? "Unknown")
+print()
 
 for (key, value) in crewRoles {
     print("\(key) is a \(value)")
