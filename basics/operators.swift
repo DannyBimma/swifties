c2cf51c59700600cd98c29edc3fd92a5050bd3c6
DannyBimma <dantrotman92@gmail.com>
2025-12-20 21:26:39 -0400

    iTired.

--- basics/operators.swift
+++ basics/operators.swift
@@ -1,6 +1,21 @@
 /*
 * Routine: This source code displays the basics of operators in Swift.
 * Author:  DannyBimma
-* Date:    December, 2025
+* Date:    2025-12-20
 * Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
 */
+
+// Assign program variables
+let count = 69  // This is a constant value
+var count2 = 420
+count2 = count2 + count2
+
+print("count: \(count)")
+print("count2: \(count2)")
+
+// Assign multiple values to multiple variables using a tuple
+let (x, y, z) = (123, 234, 345)
+
+print("x: \(x)")
+print("y: \(y)")
+print("z: \(z)")
