01c0af3d290ae1f219d488b8b3478124b7c55983
DannyBimma <dantrotman92@gmail.com>
2025-12-31 16:36:45 -0400

    Init Happy New Year program

---
+++ newyear.swift
@@ -0,0 +1,10 @@
+/*
+Routine:  A program that prints a beautiful Happy New Year's message
+          to the terminal using ASCII art.
+
+Author:   DannyBimma
+
+Date:     December, 2025.
+
+Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
+*/
