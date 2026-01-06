74b066ce43dcb38a5771646ebf1df4b537fa84ab
DannyBimma <dantrotman92@gmail.com>
2026-01-05 21:14:55 -0400

    String literals

--- basics/strings.swift
+++ basics/strings.swift
@@ -1,7 +1,32 @@
 /*
-* Routine: This source code displays the power of strings in Swift.
+* Routine: This source code displays the power of strings and chars in Swift.
 *
 * Author:  DannyBimma
 *
 * Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
 */
+
+// NOTE: String Literals
+print("\n")
+print("STRING LITERALS")
+
+let callSign = "Technomancer Pirate Captain"
+
+print("Call Sign: \(callSign)")
+
+// NOTE: Multiline String Literals
+print("\n")
+print("MULTILINE STRING LITERALS")
+
+let stringMessage = """
+    If you need a string that spans several lines in your program;
+    or you simply want a very long message to be printed to the
+    console without having to scroll horizontally, Swift allows
+    you to use multiline string literals for exaxctly this.
+
+    Another super awesome benefit; is how it allows you to have
+    the format of a long message or story in your source code be
+    perfectly preserved in the output of your program.
+    """
+
+print(stringMessage)
