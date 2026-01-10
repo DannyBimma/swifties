49ea7a6e1687fc4089c2004df61e707ee96fd195
DannyBimma <dantrotman92@gmail.com>
2026-01-10 16:00:39 -0400

    Escapes and special chars

--- basics/strings.swift
+++ basics/strings.swift
@@ -10,7 +10,7 @@
 print("\n")
 print("STRING LITERALS")
 
-let callSign = "Technomancer Pirate Captain"
+let callSign = "Technomancer Pirate Captain \u{1F3F4}\u{200D}\u{2620}"  // Escape for Unicode
 
 print("Call Sign: \(callSign)")
 
@@ -22,11 +22,18 @@ let stringMessage = """
     If you need a string that spans several lines in your program;
     or you simply want a very long message to be printed to the
     console without having to scroll horizontally, Swift allows
-    you to use multiline string literals for exaxctly this.
+    you to use multiline string literals for exaxctly this \u{1F44D}\u{1F3FE}
 
-    Another super awesome benefit; is how it allows you to have
+    Another super awesome benefit: is how it allows you to have
     the format of a long message or story in your source code be
-    perfectly preserved in the output of your program.\n
+    perfectly preserved in the output of your program \u{1F4BB}
+
+    The Swift programming language fucks \u{1F60E}!!\n
+
+    P.s. You can also just include emoji directly instead of
+    using the Unicode escape sequences... Swift just goes that
+    fucking hard 😮‍💨
+
     """
 
 print(stringMessage)
