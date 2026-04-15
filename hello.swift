832a55d82f0828a05cc2b89d8ea1fa8c460fdf6f
DannyBimma <dantrotman92@gmail.com>
2026-04-15 10:57:10 -0400

    changes.

--- hello.swift
+++ hello.swift
@@ -1,19 +1,31 @@
+/*
+* Routine: The canonical "Hello, World!" program used to introduce
+*          a programming language.
+*
+* Author:  DannyBimma
+*
+* Date:    Apr, 2026
+*
+* Copyright (c) 2026 Technomancer Pirate Captain. All Rights Reserved.
+*/
+
 // The first line of code written in global scope is used as the entry
-// point to a Swift program.
+// point to a Swift program; and it supports both styles of traditional
+// C comments.
 
 import Foundation
 
-print("\n")  // new line character is not blessed by the syntax highlighting gods... damn shame.
+print("\n")  // the print function auto inserts a newline, so this statement outputs 2
 
-// Constants are declared with the let keyword and are immutable.
+// Constants are declared with the let keyword.
 let myName = "DannyBimma"
 let title = "the Technomancer Pirate Captain"
-let declaration = "the man who will become King of the iOS Developers"
+let declaration = "the man who will become King of the Apple Developers"
 
 let currentYear = Calendar.current.component(.year, from: Date())
 let publicationYear = 1997
 
-// Variables are declared with the var keyword and are mutable.
+// Variables are declared with the var keyword.
 var publicationRun: Int
 publicationRun = currentYear - publicationYear
 
@@ -31,9 +43,8 @@ print(
 print()
 
 // Values in Swift are never implicitly converted to another type.
-// You must be explicit widdit.
-let rating: Float = 9
+let rating: Float = 9  // you must be explicit widdit.
 
-print("My shōnen manga rating: \(rating)")
+print("My shōnen manga rating: \(rating)")  // this outputs a floating point number
 
 print("\n")
