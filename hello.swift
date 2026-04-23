2d4fc3a1f50e2d0b017c14e32c12ab657c27d91e
DannyBimma <dantrotman92@gmail.com>
2026-04-23 19:17:41 -0400

    arrays and dictionaries.

--- hello.swift
+++ hello.swift
@@ -46,12 +46,46 @@ print(
 
 print()
 
-// Values in Swift are never implicitly converted to another type.
-let rating: Float = 9  // you must be explicit widdit.
-let decade: Float = 10
-let andThenSome: Float = 7.5
+let mangaRating: Float = 10  // Values in Swift are never implicitly converted
+let animeRating: Float = 8.5  // Ya must be explicit widdit
+let animeRatingAdjustment: Float = 2.5
 
-print("My shōnen manga rating: \(rating)")  // this outputs a floating point number
-print("My anime show run: \((decade * 2) + andThenSome) (years)")
+print("Shōnen manga rating: \(mangaRating)")  // This outputs a floating point number
+print("Anime show rating: \((animeRating + mangaRating) / animeRatingAdjustment)")
+
+print()
+
+/*
+Arrays and Dicts are initialised with square bracket notation and
+their elements are accessed using an index or key respectively.
+You are also allowed to place a comma after the last element
+for reasons.
+*/
+var strawatCrew = [
+    "Roronoa Zolo", "Nami", "Usopp", "Vinsmoke Sanji", "Tony Tony Chopper", "Nico Robin", "Franky",
+    "Brook", "Jinebe",
+]
+
+strawatCrew.insert("Monkey D. Luffy", at: 0)  // Automatically grows as elements are added
+
+var aliases = [
+    strawatCrew[0]: "Straw Hat", strawatCrew[1]: "Pirate Hunter", strawatCrew[2]: "Cat Burglar",
+    strawatCrew[3]: "God",
+    strawatCrew[4]: "Black leg", strawatCrew[5]: "Chopper", strawatCrew[6]: "Devil Child",
+    strawatCrew[7]: "Franky", strawatCrew[8]: "Soul King", strawatCrew[9]: "First Son of the Sea",
+]
+
+var crewRoles = [
+    aliases["Monkey D. Luffy"]: "Captain", aliases["Roronoa Zolo"]: "Swordsman (Monster Trio)",
+    aliases["Nami"]: "Navigator", aliases["Usopp"]: "Sniper/Tech Lead",
+    aliases["Vinsmoke Sanji"]: "Cook", aliases["Tony Tony Chopper"]: "Doctor",
+    aliases["Nico Robin"]: "Archaeologist", aliases["Franky"]: "Shipwright",
+    aliases["Brook"]: "Musician", aliases["Jinebe"]: "Helmsman",
+]
+
+/*
+Swift uses "if" and "switch" statements for conditionals. And "for-in",
+"while" and "repeat-while" statements for loops.
+*/
 
 print("\n")
