99d2decb9a0de631b60c2d6de0d91b19511e01c2
DannyBimma <dantrotman92@gmail.com>
2026-06-15 22:49:01 -0400

    write funky goal tally prog.

---
+++ example-progs/funky.swift
@@ -0,0 +1,34 @@
+/*
+* Routine: A program that keeps a record of goals scored in the 2026 World Cup.
+*
+* Author:  DannyBimma
+*
+* Copyright (c) 2026 Technomancer Pirate Captain. All Rights Reserved.
+*/
+
+func goalRecords(goals: [Int]) -> (leastGoals: Int, mostGoals: Int, totalGoals: Int) {
+    var leastGoals = goals[0]
+    var mostGoals = goals[0]
+    var totalGoals = 0
+
+    for currentGoal in goals {
+        if currentGoal > mostGoals {
+            mostGoals = currentGoal
+        } else if currentGoal < leastGoals {
+            leastGoals = currentGoal
+        }
+
+        totalGoals += currentGoal
+    }
+
+    return (leastGoals, mostGoals, totalGoals)
+}
+
+let worldCupGoals = goalRecords(goals: [
+    2, 0, 2, 1, 1, 1, 4, 1, 1, 1, 1, 1, 0, 1, 2, 0, 7, 1, 2, 2, 1, 0, 5, 1, 0, 0, 1, 1, 1, 1, 1, 1,
+])
+
+print("===GOALS SCORED IN THE 2026 WORLD CUP===")
+print("Most: \(worldCupGoals.mostGoals)")
+print("Least: \(worldCupGoals.leastGoals)")
+print("Total: \(worldCupGoals.totalGoals)")
