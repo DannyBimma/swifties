60dd0781b6a655f99cc25f86ade2d5930ad68c38
DannyBimma <dantrotman92@gmail.com>
2026-06-26 11:31:18 -0400

    modify match processor to extract and output most goals per match.

--- basic-progs/wcg.swift
+++ basic-progs/wcg.swift
@@ -156,10 +156,12 @@ worldCupGoals.append(0)
 // Goals on 26/06/2026
 //TODO:
 
-func matchProcessor(matches: [Int]) -> (matchCount: Int, goalessDraws: Int) {
+func matchProcessor(matches: [Int]) -> (matchCount: Int, goalessDraws: Int, mostGoals: Int) {
     var teamMatchCount = 0
     var goalessDraws = 0
     var matchCount = 0
+    var matchGoals = 0
+    var mostGoals = 0
 
     for _ in matches {
         teamMatchCount += 1
@@ -173,7 +175,15 @@ func matchProcessor(matches: [Int]) -> (matchCount: Int, goalessDraws: Int) {
         }
     }
 
-    return (matchCount, goalessDraws)
+    for (current, next) in zip(matches, matches.dropFirst()) {
+        matchGoals = current + next
+
+        if matchGoals > mostGoals {
+            mostGoals = matchGoals
+        }
+    }
+
+    return (matchCount, goalessDraws, mostGoals)
 }
 
 func goalProcessor(goals: [Int]) -> (goalMin: Int, goalmax: Int, gotalTotal: Int) {
@@ -206,5 +216,6 @@ print("Matches Played: \(matchTally.matchCount)")
 print("Goaless Draws: \(matchTally.goalessDraws)")
 print("Most Goals (per team): \(goalTally.goalmax)")
 print("Least Goals (per team): \(goalTally.goalMin)")
+print("Most Goals (per match): \(matchTally.mostGoals)")
 print("Total Goals (in tournament): \(goalTally.gotalTotal)")
 print()
