28d0ca5331164f8107519cab139c6231694b4865
DannyBimma <dantrotman92@gmail.com>
2026-07-01 16:42:37 -0400

    add sets work.

--- example-progs/arrs_n_dicts.swift
+++ example-progs/arrs_n_dicts.swift
@@ -27,14 +27,41 @@ var distinctEvenNums: Set<Int> = [0, 2, 4, 6, 8, 16, 14]
 
 distinctEvenNums.insert(420)
 
-if distinctEvenNums.isEmpty {
-    print("Nobody has sets in this city.")
-} else {
-    print("This set of distinct even numbers contains \(distinctEvenNums.count) integers:")
+print("This set of distinct even numbers contains \(distinctEvenNums.count) integers:")
 
-    for nums in distinctEvenNums {
-        print(nums)
-    }
+for nums in distinctEvenNums {
+    print(nums)
 }
 
+let dopeNums = Set(0...420)
+let distinctOddNums = Set([1, 3, 5, 7, 13, 15, 419])
+
+print()
 // Set Operations
+// Create a new set with values common to two different sets
+let distinctNums = distinctEvenNums.union(distinctOddNums).sorted()
+
+// Create a new set with only the values common to two different sets
+let houseOfCommons = distinctEvenNums.intersection(distinctOddNums)
+
+if houseOfCommons.isEmpty {
+    print("The House of Commons? Nobody has sets in that house!")
+} else {
+    print("A bit cheeky innit?")
+
+}
+
+// Check if all the values of one set are contained in another
+if distinctEvenNums.isSubset(of: dopeNums) {
+    print("Even numbers? Dope!")
+}
+
+// Check if a set contains all the values of another
+if dopeNums.isSuperset(of: distinctEvenNums) {
+    print("Dope? Even numbers are!")
+}
+
+// NOTE: DICTIONARIES
+print()
+print("===DICTIONARIES IN THE LIBRARY===")
+print()
